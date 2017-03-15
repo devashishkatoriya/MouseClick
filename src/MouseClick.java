9fc07ada3f40973c15cb8fc071fc4e33eff9b495
Devashish <devashishkatoriya@gmail.com>
2017-03-15 13:44:12 +0530

    Initial commit

---
+++ src/MouseClick.java
@@ -0,0 +1,76 @@
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+ * Created by Devashish Katoriya on 15/3/17.
+ */
+public class MouseClick extends Applet implements MouseListener,MouseMotionListener {
+
+    private int n,xx[],yy[];
+    private static final int LIMIT = 30;
+    public void mouseClicked(MouseEvent m)
+    {
+        // TODO Auto-generated method stub
+
+        xx[n]=m.getX();
+        yy[n]=m.getY();
+        n++;
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+        setBackground(Color.YELLOW);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+        setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    public void init()
+    {
+        addMouseListener(this);
+        addMouseMotionListener(this);
+
+        setBackground(Color.WHITE);
+
+        xx = new int[LIMIT];
+        yy = new int[LIMIT];
+        n = 0;
+
+    }
+
+    public  void paint(Graphics g)
+
+    {
+        if(n>=2)
+        {
+            g.drawPolygon(xx,yy,n);
+
+            g.fillPolygon(xx,yy,n);
+        }
+
+    }
+    @Override
+    public void mouseDragged(java.awt.event.MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(java.awt.event.MouseEvent mouseEvent) {
+
+    }
+}
